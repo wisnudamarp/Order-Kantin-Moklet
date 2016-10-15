68a2ad5aec7a02db22a2afa80b3997e33a76f64f
Wisnu <wisnudamar001@gmail.com>
2016-10-15 22:47:53 +0700

    Update Commit : 1. Mendesain dan mengatur layout 2. Membuat variabel dan menyambungkan id di layout

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3039/ordermakanankantin/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3039/ordermakanankantin/MainActivity.java
@@ -15,6 +15,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    TextView tvHasil;
+    EditText edNama, edKelas, edNo;
+    Spinner spKantin;
+    CheckBox cb11, cb12, cb21, cb22, cb23;
+    RadioGroup rgMinum;
+    String cbHasil1="";
+    String cbHasil2="";
 
 
     @Override
@@ -22,6 +29,23 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Kantin SMK Telkom Malang");
+        getSupportActionBar().setSubtitle("Pesan Makan");
+
+        edNama = (EditText) findViewById(R.id.editTextNama);
+        edKelas = (EditText) findViewById(R.id.editTextKelas);
+        edNo = (EditText) findViewById(R.id.editTextNo);
+        spKantin = (Spinner) findViewById(R.id.spinnerKantin);
+        cb11 = (CheckBox) findViewById(R.id.checkBoxNs);
+        cb12 = (CheckBox) findViewById(R.id.checkBoxSy);
+        cb21 = (CheckBox) findViewById(R.id.checkBoxTl);
+        cb22 = (CheckBox) findViewById(R.id.checkBoxTm);
+        cb23 = (CheckBox) findViewById(R.id.checkBoxTh);
+        rgMinum = (RadioGroup) findViewById(R.id.radioGroupMinum);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+
 
     }
 
