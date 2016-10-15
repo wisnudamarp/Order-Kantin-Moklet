a7318fbcac29b5cad38009902c3ea473d907a7b8
Wisnu <wisnudamar001@gmail.com>
2016-10-15 22:46:41 +0700

    Update Commit : 1. Mendesain dan mengatur layout

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3039/ordermakanankantin/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3039/ordermakanankantin/MainActivity.java
@@ -1,13 +1,28 @@
 package id.sch.smktelkom_mlg.tugas01.xiirpl3039.ordermakanankantin;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
